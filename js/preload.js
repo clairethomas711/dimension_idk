c34d62c3e3a0ea63c48344b774ea255554b38ad9
Millmoss <omelliff@yahoo.com>
2017-10-11 16:44:52 -0400

    Pushing not really working but working I guess code for platforms

---
+++ js/preload.js
@@ -0,0 +1,30 @@
+let preloadState = function() {
+	
+}
+
+preloadState.prototype.preload = function() {
+	game.load.spritesheet("platform3D", "assets/FantasyPlatform3D.png", 256, 256);
+	game.load.spritesheet("shrek", "assets/dude.png", 32, 48);
+	game.load.spritesheet("doddy", "assets/Doddy.png", 96, 132);
+	
+	
+	// Load the background
+	game.load.image("fantasy_bg1", "Backgrounds/level1/Background_Fantasy_1.png");
+	game.load.image("fantasy_bg2", "Backgrounds/level1/Background_Fantasy_2.png");
+	game.load.image("fantasy_bg3", "Backgrounds/level1/Background_Fantasy_3.png");
+	game.load.image("fantasy_bg4", "Backgrounds/level1/Background_Fantasy_4.png");
+	game.load.image("fantasy_bg5", "Backgrounds/level1/Background_Fantasy_5.png");
+	
+	// Load the levels and tilesets
+	game.load.tilemap('level1', 'assets/test.json', null, Phaser.Tilemap.TILED_JSON);
+	this.load.image('level1tiles', 'assets/tiletest.png');
+	
+}
+
+preloadState.prototype.create = function() {
+	game.state.start("Gameplay");
+}
+
+preloadState.prototype.update = function() {
+	
+}
