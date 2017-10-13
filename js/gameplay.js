6da814ebbd650188eb8ff411406bea3302403a38
Claire Thomas <clairethomas711@gmail.com>
2017-10-13 11:12:52 -0400

    Added a shiny new idle animation (that still needs tweaking) and implimented

--- js/gameplay.js
+++ js/gameplay.js
@@ -128,10 +128,10 @@ gameplayState.prototype.create = function() {
 	
 	game.physics.arcade.enable(this.platform3DGroup);
 	//end platform code
-	this.player.animations.add("walk", [3, 4, 5, 6], 10, true);
+	this.player.animations.add("walk", [7, 8, 9, 10, 11, 12, 13, 14], 10, true);
 	//this.player.animations.add("right", [5, 6, 7, 8], 10, true);
-	this.player.animations.add("idle", [0,1], 5, true);
-	this.player.animations.add("jump", [2], 10, false);
+	this.player.animations.add("idle", [0, 1, 2, 3, 4, 5], 10, true);
+	this.player.animations.add("jump", [6], 10, false);
 	
 	this.cursors = game.input.keyboard.createCursorKeys();
 }
