87b5f6a05224d7323490907eb8560995f1d74b0f
Claire Thomas <clairethomas711@gmail.com>
2017-10-17 17:07:43 -0400

    Added the WEast doddy!

--- js/preloadTwoState.js
+++ js/preloadTwoState.js
@@ -4,6 +4,7 @@ let preloadTwoState = function() {
 
 preloadTwoState.prototype.preload = function() {
 	game.load.spritesheet("platform3D", "assets/WestPlatform3D.png", 290, 290);
+	game.load.spritesheet("doddy", "assets/West_Doddy.png", 96, 132);
 	
 	// Load the background
 	game.load.image("weast_bg1", "Backgrounds/level2/Background_Weast_1.png");
