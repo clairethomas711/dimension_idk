0fcde68cfbf931b0ecc069ce2b0cc7e164b7af11
CJ Desktop <legatc@rpi.edu>
2017-10-18 09:36:32 -0400

    title cards fade out instead, so thats cool

--- js/levelThreeState.js
+++ js/levelThreeState.js
@@ -814,7 +814,7 @@ levelThreeState.prototype.pressButton = function(player, button) {
 levelThreeState.prototype.playCutscene = function() {
 	switch(this.cutsceneIndex) {
 		case -1: {
-			this.title.kill();
+			game.add.tween(this.title).to( { alpha: 0 }, 600, Phaser.Easing.Linear.None, true);
 			this.transition.animations.play("open");
 			this.cutsceneIndex += 1;
 			this.inCutscene = false
